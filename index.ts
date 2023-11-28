37f70273a5efefb9917c0b935d2b3572c2db4150
Mierlut Timotei <mierluttimotei@Mierluts-MacBook-Pro.local>
2023-11-28 17:53:22 +0200

    First experimental branch commit

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
+import { formatDate } from './date';
 const express = require('express');
 import { Express, Request, Response } from 'express';
 const app: Express = express();
 require('dotenv').config();
 
+function logger(req: Request, res: Response, next: any): void {
+    console.log(`${formatDate(new Date)} - ${req.method} - ${req.url}`);
+    next();
+} app.use(logger);
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!')
+})
+
+app.get("/test", (req: Request, res: Response) => {
+    res.json({ok: true});
+})
+
+app.get("/guests/:name", (req: Request, res: Response) => {
+    res.send(`Hi ${req.params.name}!`);
 })
 
 app.listen(process.env.PORT, () => {
